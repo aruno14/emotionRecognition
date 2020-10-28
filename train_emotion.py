6f681a06ad50b7aa8784b9f45b3722fd356b071d
arnaud <admin@alpaga.ovh>
2020-10-28 23:22:54 +0900

    Load model to continue learning

--- train_emotion.py
+++ train_emotion.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+import os
+import matplotlib.pyplot as plt
 
 image_size = (48, 48)
 batch_size = 32
 epochs = 15
+model_name = "emotion_model"
 
 train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2, horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
@@ -25,8 +29,19 @@ validation_generator = test_datagen.flow_from_directory(
     class_mode='categorical')
 
 print(train_generator.class_indices)
-classifier = tf.keras.applications.mobilenet_v2.MobileNetV2(include_top=True, weights=None, input_tensor=None, input_shape=image_size + (1,), pooling=None, classes=7)
+if os.path.exists(model_name):
+    print("Load: " + model_name)
+    classifier = load_model(model_name)
+else:
+    classifier = tf.keras.applications.mobilenet_v2.MobileNetV2(include_top=True, weights=None, input_tensor=None, input_shape=image_size + (1,), pooling=None, classes=7)
 classifier.compile(loss='categorical_crossentropy', metrics=['accuracy'])
 
-classifier.fit(train_generator, steps_per_epoch=train_generator.samples//batch_size, epochs=epochs, validation_data=validation_generator, validation_steps=validation_generator.samples//batch_size)
-classifier.save("emotion_model")
+history = classifier.fit(train_generator, steps_per_epoch=train_generator.samples//batch_size, epochs=epochs, validation_data=validation_generator, validation_steps=validation_generator.samples//batch_size)
+classifier.save(model_name)
+
+metrics = history.history
+plt.plot(history.epoch, metrics['loss'], metrics['accuracy'])
+plt.legend(['loss', 'acc'])
+plt.savefig("learning-emotion.png")
+plt.show()
+plt.close()
